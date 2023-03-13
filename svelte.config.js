4c022f763dfe1b4d87770083bba5137a4b54e6cb
Natalie <ngereluk1@gmail.com>
2023-03-12 19:34:45 -0600

    possibly fixing adapter

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,7 @@ import vercel from "@sveltejs/adapter-vercel";
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
-  //preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
   kit: {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
