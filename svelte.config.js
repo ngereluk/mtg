6bc0ed657fac28b320ffd149a8a2c3ed04afc8ea
Natalie <ngereluk1@gmail.com>
2023-03-12 19:41:04 -0600

    possibly fixing adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 /** @type {import('@sveltejs/kit').Config} */
 import vercel from "@sveltejs/adapter-vercel";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
